4de5a5987030920b533a58e981d95fafa8cc3403
somaia <zeinabzaid326@gmail.com>
2021-03-25 13:21:44 +0000

    inital commit

---
+++ flutter_app_1/lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+  //  var mdw = MediaQuery.of(context).size.width;
+  //  var mdh = MediaQuery.of(context).size.height;
+
+
+    return MaterialApp(
+      title: 'first app',
+      home: Scaffold(
+       body: Stack(
+         children: [
+           Container(height:double.infinity,width: double.infinity,color:Colors.deepPurple,),
+           Container(height:400 ,width: 400,color: Colors.white,)
+         ],
+       ),
+    ),
+        
+        
+
+
+);
+
+  }
+}
